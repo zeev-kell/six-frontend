d9410eda213ebe615ba98d8bab69783f81602298
keziyuan <keziyuan@genomics.cn>
2021-03-02 01:37:29 +0800

    fix: 视图层: 修改mark解析问题，增加cwl-tool的字符判断解析

--- directives/marked.js
+++ directives/marked.js
@@ -4,9 +4,9 @@ import sanitizeHtml from 'sanitize-html'
  * 转换 markdown 语法
  */
 function translateMark(el, binding) {
-  const html = marked.parseInline(binding.value.replace(/[↵ ]{2,}/g, '  \n'), { smartypants: true })
+  const html = marked(binding.value.replace(/[↵ ]{2,}/g, '  \n'), { smartypants: true })
   // eslint-disable-next-line no-console
-  console.log(html)
+  // console.log(html)
   // 首个P元素需要重置一下margin属性
   if (/^<p[> ]/.test(html)) {
     el.classList.add('marked-p')
--- pages/application/_components/cwlTool.vue
+++ pages/application/_components/cwlTool.vue
@@ -1,17 +1,39 @@
-<template>
-  <div class="workflow-tool">
-    <el-button-group>
-      <el-button type="dark" size="mini" icon="el-icon-plus" title="放大" @click="upscale" />
-      <el-button type="dark" size="mini" icon="el-icon-minus" title="缩小" @click="downscale" />
-      <el-button type="dark" size="mini" icon="iconfont icon-fullscreen" title="适应窗口" @click="fitToViewport" />
-    </el-button-group>
-    <el-button type="dark" size="mini" icon="el-icon-magic-stick" title="自动排版" @click="autoLayout" />
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="cwl-tool">-->
+<!--    <el-button-group>-->
+<!--      <el-button type="dark" size="mini" icon="el-icon-plus" title="放大" @click="upscale" />-->
+<!--      <el-button type="dark" size="mini" icon="el-icon-minus" title="缩小" @click="downscale" />-->
+<!--      <el-button type="dark" size="mini" icon="iconfont icon-fullscreen" title="适应窗口" @click="fitToViewport" />-->
+<!--    </el-button-group>-->
+<!--    <el-button type="dark" size="mini" icon="el-icon-magic-stick" title="自动排版" @click="autoLayout" />-->
+<!--  </div>-->
+<!--</template>-->
 
 <script type="text/babel">
   import { SVGArrangePlugin, Workflow } from 'cwl-svg'
   import { DeletionPlugin } from 'cwl-svg/compiled/src/plugins/deletion/deletion'
+  const BUTTON_LIST = {
+    plus: {
+      icon: 'el-icon-plus',
+      title: '放大',
+      click: 'upscale',
+    },
+    minus: {
+      icon: 'el-icon-minus',
+      title: '缩小',
+      click: 'downscale',
+    },
+    fit: {
+      icon: 'iconfont icon-fullscreen',
+      title: '适应窗口',
+      click: 'fitToViewport',
+    },
+    auto: {
+      icon: 'el-icon-magic-stick',
+      title: '自动排版',
+      click: 'autoLayout',
+    },
+  }
 
   export default {
     name: 'CwlTool',
@@ -20,9 +42,9 @@
         type: Workflow,
         default: null,
       },
-      tool: {
-        type: String,
-        default: '',
+      tools: {
+        type: [String, Array],
+        default: 'plus,minus,fit|auto',
       },
       readonly: {
         type: Boolean,
@@ -63,18 +85,57 @@
         this.graph.getPlugin(DeletionPlugin).deleteSelection()
       },
     },
+    render(createElement) {
+      const tools = this.tools.split('|')
+      const createButton = (b) => {
+        const btn = BUTTON_LIST[b]
+        return createElement('el-button', {
+          // 普通的 HTML attribute
+          attrs: {
+            title: btn.title,
+          },
+          // 组件 prop
+          props: {
+            type: 'dark',
+            size: 'mini',
+            icon: btn.icon,
+          },
+          ...BUTTON_LIST[b],
+          on: {
+            click: this[btn.click],
+          },
+        })
+      }
+      return createElement(
+        'div',
+        { class: 'cwl-tool' },
+        tools.map((t) => {
+          if (t.includes(',')) {
+            t = t.split(',')
+            return createElement('el-button-group', t.map(createButton))
+          }
+          return createButton(t)
+        })
+      )
+    },
   }
 </script>
 
 <style scoped lang="scss" rel="stylesheet">
-  .workflow-tool {
+  .cwl-tool {
     position: absolute;
     right: 30px;
     top: 10px;
     z-index: 10;
     color: white;
+    > .el-button {
+      vertical-align: middle;
+    }
   }
   .el-button--mini {
     padding: 8px;
+  }
+  .el-button-group + .el-button {
+    margin-left: 4px;
   }
 </style>
