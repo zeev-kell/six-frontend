ee7c425ac0e61f729b3fc1a48fac855ae9dbfb32
keziyuan <keziyuan@genomics.cn>
2021-05-03 01:03:43 +0800

    feat: 视图层: 修改 getter 异常

--- store/getters.js
+++ store/getters.js
@@ -1,8 +1,8 @@
 const getters = {
   username: (state) => state.auth?.user?.uname,
   userId: (state) => state.auth?.user?.uid,
-  loggedIn: (state) => state.auth.loggedIn,
-  role: (state) => state.auth.user.admin,
-  permissions: (state) => state.auth.user.permissions,
+  loggedIn: (state) => state.auth?.loggedIn,
+  role: (state) => state.auth?.user?.admin,
+  permissions: (state) => state.auth?.user?.permissions,
 }
 export default getters
