eb5511585a66119a7ced784a147cb129608dac6a
keziyuan <keziyuan@genomics.cn>
2021-08-09 01:51:27 +0800

    fix: 构建层: 修改构建css

--- store/pipe.ts
+++ store/pipe.ts
@@ -1,60 +1,46 @@
 /* eslint-disable camelcase */
-import { Module, VuexModule, VuexMutation } from 'nuxt-property-decorator'
+import { GetterTree, MutationTree } from 'vuex'
 import PipeConstants from '@/constants/PipeConstants'
+import { NuxtState } from '@nuxt/types/app'
 
-@Module({
-  name: 'pipe',
-  stateFactory: true,
-  namespaced: true,
+export const state = () => ({
+  pipe_id: null,
+  name: null,
+  type: 0,
+  provider: null,
+  website: null,
+  description: null,
+  readme: null,
+  resource_id: null,
+  version: null,
+  dockerfile: null,
+  instruction: null,
+  cwl: null,
+  profile: null,
+  content: null,
+  create_at: null,
+  updated_at: null,
+  category: null,
+  versions: [],
 })
-export default class PipeModule extends VuexModule {
-  pipe_id = null
-  name = null
-  type = 0
-  provider = null
-  website = null
-  description = null
-  readme = null
-  resource_id = null
-  version = null
-  dockerfile = null
-  instruction = null
-  cwl = null
-  profile = null
-  content = null
-  create_at = null
-  updated_at = null
-  category = null
-  versions = []
 
-  get isTool() {
-    return this.type === PipeConstants.Constants.get('TYPE_TOOL')
-  }
-  get isApp() {
-    return this.type === PipeConstants.Constants.get('TYPE_APP')
-  }
-  get isDocker() {
-    return PipeConstants.Constants.get('TYPE_DOCKER') === this.type
-  }
-  get isWork() {
-    return PipeConstants.Constants.get('TYPE_WORK') === this.type
-  }
-  get isWorkflow() {
-    return PipeConstants.Constants.get('TYPE_WORKFLOW') === this.type
-  }
+export type PipeModuleState = ReturnType<typeof state>
+
+export const getters: GetterTree<PipeModuleState, NuxtState> = {
+  isTool: (state) => state.type === PipeConstants.Constants.get('TYPE_TOOL'),
+  isApp: (state) => state.type === PipeConstants.Constants.get('TYPE_APP'),
+  isDocker: (state) => state.type === PipeConstants.Constants.get('TYPE_DOCKER'),
+  isWork: (state) => state.type === PipeConstants.Constants.get('TYPE_WORK'),
+  isWorkflow: (state) => state.type === PipeConstants.Constants.get('TYPE_WORKFLOW'),
   // 是否是软件应用
-  get isSoftware() {
-    return [PipeConstants.Constants.get('TYPE_TOOL'), PipeConstants.Constants.get('TYPE_APP')].includes(this.type)
-  }
-  // 是否是运行工作
-  get isOperation() {
-    return [PipeConstants.Constants.get('TYPE_WORK'), PipeConstants.Constants.get('TYPE_WORKFLOW')].includes(this.type)
-  }
+  isSoftware: (state) => [PipeConstants.Constants.get('TYPE_TOOL'), PipeConstants.Constants.get('TYPE_APP')].includes(state.type),
+  isOperation: (state) => [PipeConstants.Constants.get('TYPE_WORK'), PipeConstants.Constants.get('TYPE_WORKFLOW')].includes(state.type),
+}
 
-  @VuexMutation
+export const mutations: MutationTree<NuxtState> = {
   UPDATE_CURRENT_WORKFLOW(item: any) {
     Object.keys(item).forEach((key) => {
       ;(this as any)[key] = item[key]
     })
-  }
+  },
 }
